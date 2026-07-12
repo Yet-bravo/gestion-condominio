fa1ea75583752939f665ba55699a88f81bd1f5c5
YetBravoSisprot <ybravo@sisprotgf.com>
2026-07-11 21:42:49 -0400

    Allow Vite to load standard SUPABASE_ environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ import { createClient } from '@supabase/supabase-js';
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl || '', supabaseKey || '');
 
